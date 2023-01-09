f14c7ead39a81a12fe025ee609fb6e19ae414bc8
mmilton <murray.l.milton@outlook.com>
2023-01-09 14:08:25 +0100

    added the function example for chaining functions.

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -11,7 +11,7 @@
 // const if = 23;
 
 // Functions
-
+/*
 function logger() {
   console.log("Logging data file");
 }
@@ -20,4 +20,85 @@ function logger() {
 logger();
 logger();
 logger();
-logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples, and ${oranges} oranges`;
+  return juice;
+}
+
+const juice = fruitProcessor(3, 5); //Passing the arguments into the parameters
+console.log(juice);
+// let newJuice = fruitProcessor(3, 5);
+// console.log(newJuice);
+// console.log(fruitProcessor(4, 5));
+*/
+
+// function declaration
+/*
+function calAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calAge1(1991);
+
+//Function Expression // Anonymous Function function without a name
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1992);
+console.log(age2, age1);
+*/
+
+// Arrow Functions
+
+/*
+const calAge3 = (birthYear) => 2037 - birthYear;
+const newAge = calAge3(1991);
+console.log(newAge);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //   return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Murray"));
+
+
+
+
+*/
+
+// function cutFruitPieces(fruit, pieces) {
+//   return fruit * pieces;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples, 2);
+//   const orangePieces = cutFruitPieces(oranges, 4);
+//   const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces. `;
+//   return juice;
+// }
+
+// console.log(fruitProcessor(3, 7));
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement <= 0) {
+    return `${firstName} has already retired`;
+  } else {
+    return `${firstName} has ${retirement} years until retirement starts`;
+  }
+};
+
+console.log(yearsUntilRetirement(1991, "Thomas"));
+console.log(yearsUntilRetirement(1965, "Rick"));
