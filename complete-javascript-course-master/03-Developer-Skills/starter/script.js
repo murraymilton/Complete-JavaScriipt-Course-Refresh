616744a9285d9d09a53365cae8a7d23d421cb3af
mmilton <murray.l.milton@outlook.com>
2023-01-13 03:08:47 +0100

    added the completed coding challenge

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,5 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
-"use strict";
+("use strict");
 
-const x = "23";
-if (x === "23") console.log("23");
+// const x = "23";
+// if (x === "23") console.log("23");
+
+const printForecast = function (arr) {
+  let day = 0;
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`...${arr[i]}°C in ${(day += 1)} days...`);
+  }
+};
+
+console.log(printForecast([17, 21, 23]));
